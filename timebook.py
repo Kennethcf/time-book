4921251942c2dacc922e751ced3e9c8d2cb1e7e6
Kennethcf <Kennethcf1@gmail.com>
2021-02-13 20:28:55 +0530

    Add files via upload

---
+++ timebook.py
@@ -0,0 +1,73 @@
+
+
+
+#	this program is used for optimization of system performance
+#	this program can be used to manage python script
+#	running your python scripts is automated
+
+
+
+
+
+#creater kenneth c f
+#creater kenneth c f
+#creater kenneth c f
+
+
+
+
+#import your process here 
+#example try.py as import try ( if in the same folder else) import (location of try.py)
+
+from datetime import datetime
+import time
+
+
+
+time = [] #add your time here (24hours) 
+process = [] #add your process here
+
+
+
+while True:
+	#time
+
+
+	for i in time:
+		now = datetime.now()
+		current_time = now.strftime("%H:%M:%S")
+		print(current_time, time[0])
+	
+
+
+	#work
+		if current_time == time[0]:			
+	
+	#error are handled for all types of errors
+
+			try:
+			
+				process[0]
+			
+			except Exception:
+
+				try:
+					process[0]
+				
+				except Exception:
+					
+					process[0]
+
+
+	#de-alocation of resourses			
+			del process[0]
+			del time[0]
+
+
+
+
+#creater kenneth c f
+#creater kenneth c f
+#creater kenneth c f
+#creater kenneth c f
+
